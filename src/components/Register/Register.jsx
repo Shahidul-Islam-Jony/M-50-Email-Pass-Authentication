59afc6417e90c303a3a11d54f8f72cebe85b75ce
Shahidul-Islam <shahidulislam13sf@gmail.com>
2023-09-29 21:28:31 +0600

    m50-6 Login User and Accept terms and conditions

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -15,7 +15,8 @@ const Register = () => {
         e.preventDefault();
         const email = e.target.email.value;
         const password = e.target.password.value;
-        // console.log(email,password);
+        const accepted = e.target.terms.checked; // terms and condition check kora ace kina jante
+        // console.log(email,password,accepted);
 
         setRegisterError(''); // notun account toirer khetre error take ui thele sorate
         setSuccess(''); // protibar success take reset korte (ui theke sorate)
@@ -27,6 +28,10 @@ const Register = () => {
         }
         else if (!/[A-Z]/.test(password)) {
             setRegisterError('Your password should have at least one upper case characters.');
+            return;
+        }
+        else if(!accepted){
+            setRegisterError('Please accepted our terms and conditions !');
             return;
         }
 
@@ -46,25 +51,31 @@ const Register = () => {
     return (
         <div className="">
             <div className="mx-auto md:w-1/2">
-                <h2 className="text-3xl mb-8 text-center">Please Register</h2>
-                <form onSubmit={handleRegister} className=" text-center">
-                    <input className="mb-4 w-3/4 py-2 px-4" type="email" name="email" placeholder="Email Address" id="" required />
+                <h2 className="text-3xl mb-8">Please Register</h2>
+                <form onSubmit={handleRegister} className=" ">
+                    <input className="mb-4 w-full py-2 px-4" type="email" name="email" placeholder="Email Address" id="" required />
                     <br />
-                    <div className="relative">
+                    <div className="mb-4 relative">
                         <input
-                            className="mb-4 w-3/4 py-2 px-4"
+                            className="w-full py-2 px-4"
                             type={showPassword ? "text" : "password"}   //For showing password
                             placeholder="Password"
                             name="password"
                             id="" required />
-                        <span className=" absolute right-16 lg:right-28 top-2 text-2xl" onClick={() => setShowPassword(!showPassword)}>
+                        <span className=" absolute top-2 right-2 text-2xl" onClick={() => setShowPassword(!showPassword)}>
                             {
                                 showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>
                             }
                         </span>
                     </div>
+                    {/* terms and conditions er checkbox deekhate */}
+                    <div className="my-2">
+                        <input type="checkbox" name="terms" id="terms" className="text-left" /> {/*id ebong htmlFor er name same hobe */}
+                        <label className="ml-2" htmlFor="terms">Accept our <a href="#">Terms and Conditions</a></label>
+                    </div>
+
                     <br />
-                    <input className="btn btn-secondary mb-4 w-3/4" type="submit" value="Register" />
+                    <input className="btn btn-secondary mb-4 w-full" type="submit" value="Register" />
                 </form>
                 {
                     registerError && <p className="text-red-500">{registerError}</p>
